c54ce04bac6f4168d79a13f453f5276d8bb87152
goforbroke <koba.hige@gmail.com>
2014-11-28 11:19:48 +0900

    canvasvideo: Implements operator marker draw

--- canvasvideo/js/operator-canvas.js
+++ canvasvideo/js/operator-canvas.js
@@ -6,34 +6,40 @@ var wsURL = "ws://" + window.location.hostname + ":" + WS_PORT + "/" + PREFIX;
 log.i(wsURL);
 
 function websocketInit() {
-	ws = new WebSocket(wsURL);
-	ws.binaryType = 'blob';
+  ws = new WebSocket(wsURL);
+  ws.binaryType = 'blob';
 
-	ws.onopen = function() {
-		log.i('Open WebSocket conection');
-	};
+  ws.onopen = function() {
+    log.i('Open WebSocket conection');
+  };
 
-	ws.onmessage = function(elm) {
-    var axis = elm.data;
-    conn.send(axis);
-	};
+  var axes = null;
+  var axis = new Array(AXIS_ARRAY_LENGTH);
+  var x_px = 0, y_px = 0;
+  ws.onmessage = function(elm) {
+    axes = elm.data;
+    axis = elm.data.split(',');
+    x_px = axis[0];
+    y_px = axis[1];
+
+    canvasDrawAxis(x_px, y_px);
+
+    conn.send(axes);
+  };
 }
 
-function canvasInit() {
-	var canvas = $('canvas')[0];
+// 受信した座標を描画する
+function canvasDrawAxis(x_px, y_px) {
+  var canvas = $('canvas')[0];
   var ctx = canvas.getContext('2d');
-  ctx.strokeStyle = "rgb(200, 0, 0)";
-  
-  $('canvas').mousemove(function(evt) {
-    var rect = evt.target.getBoundingClientRect() ;
-    var x = evt.clientX - rect.left;
-    var y = evt.clientY - rect.top;
-    log.i(x + " " + y);
-    ctx.clearRect(0, 0, VGA_WIDTH_PX, VGA_HEIGHT_PX);
-    ctx.strokeRect(x, y, 20, 20);
-  });
 
+  ctx.lineWidth = MARKER_WIDTH;
+  ctx.strokeStyle = "rgb(204, 0, 0)";
 
+  ctx.clearRect(0, 0, VGA_WIDTH_PX, VGA_HEIGHT_PX);
+  ctx.strokeRect(x_px - MARKER_SIZE_Y/2,
+                         y_px - MARKER_SIZE_Y/2,
+                         MARKER_SIZE_X, MARKER_SIZE_Y);
 }
 
 window.addEventListener('load', websocketInit, false);
